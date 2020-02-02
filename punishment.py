2a8eb4554a8994536580b91efa905ac7c1296995
RedSun-ops <55594856+RedSun-ops@users.noreply.github.com>
2020-02-02 19:40:40 +0000

    Add files via upload

---
+++ punishment.py
@@ -0,0 +1,10 @@
+import random
+# python punishment generator
+type = random.randint(1,3)
+if type == 3:
+    print("spanking")
+if type == 2:
+    print("solitary")
+if type == 1:
+    print("force sleep")
+print(type)
